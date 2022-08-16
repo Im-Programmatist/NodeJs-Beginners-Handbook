901b51879f4c2ae4c631d4b1964f252a206e2646
Chetan Korde <chetan.korde@maximess.com>
2022-08-16 14:05:22 +0530

    Streaming module added
    file module update code
    
    REPL info added

---
+++ REPL.js
@@ -0,0 +1,32 @@
+/*
+Read − Reads user's input, parses the input into JavaScript data-structure, and stores in memory.
+
+Eval − Takes and evaluates the data structure.
+
+Print − Prints the result.
+
+Loop − Loops the above command until the user presses ctrl-c twice.
+
+Uses - Tio experiment node js code and debuge js code, 
+- it is like node js cmd(windows), you put command in it it read's it evaluate it and print result. 
+- It is same as console in browser inspect where we can put js code and run it and test it.
+
+
+The Node.js Read-Eval-Print-Loop (REPL) is an interactive shell that processes Node.js expressions. 
+The shell reads JavaScript code the user enters, 
+evaluates the result of interpreting the line of code, 
+prints the result to the user, 
+and loops until the user signals to quit.
+
+The REPL is bundled with every Node.js installation and allows you to quickly test and explore JavaScript 
+code within the Node environment without having to store it in a file.
+*/
+/**
+ * just write node and enter i n terminal - we will enter in REPL environment
+ * .help - provide commands
+ * We can run -
+ * 1. js expression - 3+3 =6 ,a = 10 console.log(a);//10
+ * 2. Multiline Code - we want to put do-while loop for that initial cond  var x = 0;
+ * we enter do and press enter then repl automatically allow you to write other mutiple lines 
+ * 3. _ underscore gives the last result value. suppose first 6 entered then - _+10 =16 
+*/
--- file.js
+++ file.js
@@ -1,7 +1,53 @@
 // import the file system module
-import { writeFile } from 'fs';
+import fs, { openSync } from 'fs';
+
+const dir = './file_module';
+if(!fs.existsSync(dir)) 
+    fs.mkdirSync(dir);
+
 //create a new text filed called task and store the sthing "buy groceries" to it
-writeFile('tasks.txt', 'buy groceries', (error) =>{
-if (error) throw error;
-console.log('The file has been saved.')
+fs.writeFile(dir+'/tasks.txt', 'hello world!', (error) =>{
+    if (error) throw error;
+    console.log('The file has been saved.')
 });
+
+if(fs.existsSync(dir+'/test.txt')) 
+    console.log("File test.txt exists");
+else
+    console.log("File test.txt not exists");
+
+fs.writeFileSync(dir+'/test.txt', "Testing write file sync method of fs module");
+
+fs.appendFileSync(dir+'/test.txt',", 3 here we have Appended data 2");
+
+const buffere_data = fs.readFileSync(dir+'/test.txt'); //buffer data received frim thisreadsync method
+/*use "utf-8"* to get string data - fs.readFileSync(dir+'/test.txt', 'utf-8') */
+console.log(buffere_data);
+const original_data = buffere_data.toString();
+console.log(original_data);
+
+// Include the fs module
+// Open file demo.txt in read mode
+fs.open(dir+'/test.txt', 'r', function (err, f) {
+    console.log("Saved! Open file!");
+});
+
+// To open file in write and read mode,
+// create file if doesn't exists.
+fs.open(dir+'/test.txt', 'w+', function (err, f) {
+   if (err) {
+      return console.error(err);
+   }
+   console.log(f);
+   console.log("File opened!!");    
+});
+
+if(fs.existsSync(dir+'/test.txt')) 
+    fs.unlinkSync(dir+'/test.txt', (err) => {
+        console.log("file test.txt deleted");
+    });
+if(fs.existsSync(dir+'/tasks.txt')) 
+    fs.unlinkSync(dir+'/tasks.txt', (err) => {
+        console.log("file tasks.txt deleted");
+    });
+
