13279d8107f97689b798f7d6432f2aa7e2e03eb0
Chetan Korde <34477252+Im-Programmatist@users.noreply.github.com>
2022-12-07 16:14:09 +0530

    Update ConcurrentRequestUsingCallbackSetTimeout.js

--- ConcurrentRequestUsingCallbackSetTimeout.js
+++ ConcurrentRequestUsingCallbackSetTimeout.js
@@ -5,35 +5,34 @@
  * all the request handle at a same time, 
  * 
  * if one request take one 5 sec then 5 request takes 25 sec to execute in series(synchronously) 
- * but in parallel(Asynchronous) we pass each request at at time with small difference in time, to settimeout it will take 5 sec to execute for each 
+ * but in conccurency(Asynchronous) we pass each request at at time with small difference in time, to settimeout it will take 5 sec to execute for each 
 */
 
 const placeRequest = (reqNumber) =>{
     console.log(`Request come to process is - ${reqNumber}`);
-    
     callAndProcessRequest(function (callWay='Async'){
-        console.time(`process-request-time`);
-        console.timeLog(`process-request-time`);
-        for(let i=0; i<1e9; i++){
-            
-        }
         console.log(`${callWay} :  Request Processed, REQ No is - , ${reqNumber}`);
-        console.timeEnd(`process-request-time`);
+        console.log('Callback Start At - ',new Date().toLocaleTimeString());
+        for(let i=0; i<1e9; i++){}
+        console.log('Callback End At - ',new Date().toLocaleTimeString());
     })
 }
 
 const callAndProcessRequest = (callback) =>{
 
     //Asynchronous call and concurrently handle all requests
-    setTimeout(callback,0);
+    //setTimeout(callback,0);
+
     //Synchronous call and all requests being processed one by one
-    //callback('Sync');
+    callback('Sync');
 }
-console.time('start-request-time');
-console.timeLog('start-request-time');
+console.warn('Program Start At - ',new Date().toLocaleTimeString());
+// console.time('start-request-time');
+// console.timeLog('start-request-time');
 placeRequest(1);
 placeRequest(2);
 placeRequest(3);
 placeRequest(4);
 placeRequest(5);
-console.timeEnd('start-request-time');
+// console.timeEnd('start-request-time');
+console.warn('Program End At - ',new Date().toLocaleTimeString());
