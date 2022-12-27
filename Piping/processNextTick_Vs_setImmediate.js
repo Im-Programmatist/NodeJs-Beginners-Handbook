0aee15e3211f39bf8437bd4c0a87fe00a6eb6be1
chetankorde <ckorde@crosscountry.com>
2022-12-27 19:13:41 +0530

    promises related content added

--- Piping/processNextTick_Vs_setImmediate.js
+++ Piping/processNextTick_Vs_setImmediate.js
@@ -4,7 +4,14 @@
  *  So,  process.nextTick() method adds the callback function to the start of the next event queue
  *  But when proccess.nextTick() method is called for the first time before the event loop is processed.
  *  Syntaxt - process.nextTick(callback);
+ *  process.nextTick() is not technically part of the event loop. 
+ *  Instead, the nextTickQueue will be processed after the current operation is completed, 
+ *  regardless of the current phase of the event loop
 */ 
+/**
+ *  Promises that may resolve or reject executes before all timer and process.nextTick,
+ *  process next tick goes in to the nextTickQueue rather than micro queue(it is attached to micro queue but other functions like promises have preference over it) where promises laid. 
+ *  micro queue has more preference than nextTickQueue. () 
 /**
  * setImmdeiate() method: 
  * Whenever we call setImmediate() method, it’s callback function is placed in the check phase of the next event queue. 
@@ -29,44 +36,81 @@
  * polling phase (fetching/taking code top to bottom)--->check Phase -->execution phase 
  * -->event loop initialization --->proccess.nextTick ---> setImmediate --->setTimeout/setInterval(based on the timer)
 */
-setTimeout(function E() {
-    console.log("1st zro timeout");
-},0);
+import fs from 'fs';
+/*Promise run before process next tick and  all timers*/
+//Promise.reject('Promise rejected!')
+// Promise.resolve('Promise resolved!')
+// .then((res)=>{console.log('then Promise', res);}) //for resolve, this will run 
+// .catch((err)=>{console.log('catch',err);}) //for Reject, this will run 
 
-setImmediate(function A() {
-    console.log("1st immediate");
-});
-  
-setImmediate(function B() {
-    console.log("2nd immediate");
-});
-  
-process.nextTick(function C() {
-    console.log("1st process");
-});
-  
-process.nextTick(function D() {
-    console.log("2nd process");
-});
+// setTimeout(function E() {
+//     console.log("1st zro timeout");
+// },0);
 
-setTimeout(function F() {
-    console.log("2st timeout");
-},7000);
+// setImmediate(function A() {
+//     console.log("1st immediate");
+// });
   
-setTimeout(function G() {
-    //doing I/O operations
-    let sum = 0;
-    while (sum < 10e3) {
-        sum = sum + 100;
-    }
-    console.log("3st zro timeout");
-},0);
+// setImmediate(function B() {
+//     console.log("2nd immediate");
+// });
+  
+// process.nextTick(function C() {
+//     console.log("1st process");
+// });
+  
+// process.nextTick(function D() {
+//     console.log("2nd process");
+// });
 
-setInterval(()=>{
-    console.log("1st Interval with 10sec time");
-},10000)
-// First event queue ends here
-console.log("program started");
+// setTimeout(function F() {
+//     console.log("2st timeout");
+// },7000);
+  
+// setTimeout(function G() {
+//     //doing I/O operations
+//     let sum = 0;
+//     while (sum < 10e3) {
+//         sum = sum + 100;
+//     }
+//     console.log("3st zro timeout");
+// },0);
+
+// setInterval(()=>{
+//     console.log("1st Interval with 10sec time");
+// },10000)
+// // First event queue ends here
+// console.log("program started");
+
+
+// process.nextTick(() => {
+//     console.log(`next Tick`);
+// });
+
+// setImmediate(() => {
+//     console.log(`Set Immediate`);
+// });
+
+// setTimeout(() => {
+//     const readFile = fs.readFile('./testPipe.txt', 'utf8', (err, data)=>{
+//         if(err) throw err;
+//         console.log(`file data -`, data);
+//     }); 
+//     console.log(`readfile \n`, readFile,'\n');
+//     console.log(`set Timeout`);
+// }, 0);
+
+
+// //Promise.reject('Promise rejected!')
+// Promise.resolve('Promise resolved!')
+// .then((res)=>{console.log('then Promise', res);}) //for resolve, this will run 
+// .catch((err)=>{console.log('catch',err);}) //for Reject, this will run 
+
+// setInterval(() =>{
+//     console.log(`set Interval`);
+// },1000);
+
+// console.log(`outside call`);
 
 /**
  * Every recursive call of process.nextTick() will prevent event loop i/o operation and there will be starvation for i/o
@@ -89,13 +133,37 @@ console.log("program started");
 // console.log(`Start Of testing process nexttick performance....`);
 
 //Example of SetImmediate - this will allow settimeout to call even in recursive calling.
-let count2 = 0;
-const cpImm = () =>{
-    console.log(`SetImmediate calling cb - ${++count2}`); //this will continouesly run not allow to run immediate and timeout at all.
-    setImmediate(cpImm);
-}
-setImmediate(cpImm);
-setTimeout(()=>{
-    console.log(`Set Timeout is called ` );
-},5000);
+// let count2 = 0;
+// const cpImm = () =>{
+//     console.log(`SetImmediate calling cb - ${++count2}`); //this will continouesly run not allow to run immediate and timeout at all.
+//     setImmediate(cpImm);
+// }
+// setImmediate(cpImm);
+// setTimeout(()=>{
+//     console.log(`Set Timeout is called ` );
+// },5000);
 
+
+function abc(){
+
+    process.nextTick(() => console.log(1));
+    //Promise.resolve().then(() => console.log(2));
+    const test = new Promise((resolve, reject) =>{
+        resolve('test');
+    });
+    test.then(() => console.log(2))
+    .catch(err => console.log(err));
+
+    Promise.resolve().then(() => {
+        console.log(3);
+        setTimeout(() => console.log('8'),0);
+        setImmediate(() => console.log('7'));
+        process.nextTick(() => console.log(4));
+        Promise.resolve().then(() => console.log(5));
+    }).then(() => {
+        console.log(6);
+    })
+
+    setTimeout(() => console.log('9'),0);
+}
+abc(); //2,3,5,6,1,4,7,9,8
